31a991964bbb1ee721449a969e1ef1ecff47fc76
Fernandes Júnior <fernaandes.jr@gmail.com>
2022-01-27 14:20:34 -0300

    Entity Product

--- backend/src/main/java/com/fernandesDev/dscatalog/resources/CategoryResourse.java
+++ backend/src/main/java/com/fernandesDev/dscatalog/resources/CategoryResourse.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.util.List;
 
 @RequestMapping(value = "/categories")
 @RestController
@@ -25,8 +24,7 @@ public class CategoryResourse {
             @RequestParam(value = "page", defaultValue = "0") Integer page,
             @RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
             @RequestParam(value = "direction", defaultValue = "ASC") String direction,
-            @RequestParam(value = "orderBy", defaultValue = "name") String orderBy
-    ){
+            @RequestParam(value = "orderBy", defaultValue = "name") String orderBy){
         PageRequest pageRequest = PageRequest.of(page,linesPerPage, Sort.Direction.valueOf(direction), orderBy);
         Page<CategoryDTO> list = service.findPaged(pageRequest);
         return ResponseEntity.ok().body(list);
