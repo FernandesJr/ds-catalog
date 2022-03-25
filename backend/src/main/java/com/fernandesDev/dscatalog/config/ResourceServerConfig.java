13730f0abc9ff5c5995d91660d3c2c822886d1a4
Fernandes Júnior <fernaandes.jr@gmail.com>
2022-03-25 11:00:06 -0300

    Configuration the Cors

--- backend/src/main/java/com/fernandesDev/dscatalog/config/ResourceServerConfig.java
+++ backend/src/main/java/com/fernandesDev/dscatalog/config/ResourceServerConfig.java
@@ -1,7 +1,10 @@
 package com.fernandesDev.dscatalog.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,6 +12,10 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import java.util.Arrays;
 
@@ -48,5 +55,28 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
         if(Arrays.asList(environment.getActiveProfiles()).contains("test")){
             http.headers().frameOptions().disable();
         }
+
+        http.cors().configurationSource(corsConfigurationSource()); //Configurando o Cors
     }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOriginPatterns(Arrays.asList("*")); //Informando qual host poderá acessar esse backend: * = liberado para todos. Porém, deve informar em qual host está o front 'https://meudominio.com' assim somente este host terá acesso
+        corsConfig.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "PATCH"));
+        corsConfig.setAllowCredentials(true);
+        corsConfig.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+        return source;
+    }
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
+        FilterRegistrationBean<CorsFilter> bean
+                = new FilterRegistrationBean<>(new CorsFilter(corsConfigurationSource()));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE); //Informando que desejamos maior prescedencia para a execução deste bean
+        return bean;
+    }
+
 }
